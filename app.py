d6985b6c4c0011fc28cd4d723fe8d7fe74ef73a0
Prakhar <shukla.prakhar415@gmail.com>
2022-05-17 11:29:08 +0530

    face detection model included

--- app.py
+++ app.py
@@ -1,78 +1,172 @@
 from email.policy import default
+from fileinput import filename
+from msilib.schema import Directory
 from turtle import title
-from flask import Flask , jsonify , request
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 from flask_marshmallow import Marshmallow
 from sqlalchemy import null
 import base64
+import urllib.request
+from werkzeug.utils import secure_filename
+import io
+import os
+from pathlib import Path
+import pathlib
+import cv2
+import numpy as np
+import face_recognition
+from PIL import Image
+from imageio import imread
 
 app = Flask(__name__)
 
+app.secret_key = "prakharshukla"
+UPLOAD_FOLDER = 'static/ImgUploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///attendanceflask.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db=SQLAlchemy(app)
-ma=Marshmallow(app)
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
 
 class Students(db.Model):
-    id = db.Column(db.Integer , primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     admission_No = db.Column(db.String(10))
-    name = db.Column(db.String(50))
-    image_known = db.Column(db.Text())
-    image_unknown = db.Column(db.Text() , default = "")
-    present_status = db.Column(db.Boolean , default = False)
-    date = db.Column(db.DateTime , default = datetime.datetime.now)
+    student_name = db.Column(db.String(50))
+    image_uploded = db.Column(db.Boolean, default=False)
+    present_status = db.Column(db.Boolean, default=False)
+    date = db.Column(db.DateTime, default=datetime.datetime.now)
 
-
-    def __init__(self , admission_No , name , image_known):
+    def __init__(self, admission_No, student_name):
         self.admission_No = admission_No
-        self.name = name
-        self.image_known = image_known
+        self.student_name = student_name
+
 
 class StudentSchema(ma.Schema):
     class Meta:
-        fields = ('id' , 'admission_No' , 'name' , 'image_known', 'image_unknown' ,'present_status' , 'date')
+        fields = ('id', 'admission_No', 'student_name',
+                  'image_uploded', 'present_status', 'date')
+
 
 student_schema = StudentSchema()
 students_schema = StudentSchema(many=True)
-        
 
 
-@app.route('/get' , methods = ['GET'])
+@app.route('/get', methods=['GET'])
 def get_student_list():
     all_students = Students.query.all()
     results = students_schema.dump(all_students)
     return jsonify(results)
 
-@app.route('/add_student' , methods = ['POST'])
+# ---Adding Student Name and Admission No list Route---
+
+
+@app.route('/add_student', methods=['POST'])
 def add_student():
     admission_No = request.json['admission_No']
-    name = request.json['name']
-    image_known = request.json['image_known']
-
-    students = Students(admission_No , name , image_known)
+    student_name = request.json['student_name']
+    students = Students(admission_No, student_name)
     db.session.add(students)
     db.session.commit()
     return student_schema.jsonify(students)
 
+# ---Adding Student Image while adding image to list Route---
+
+
+@app.route('/add_student/image_upload/<admission_No>/', methods=['POST'])
+def add_student_image(admission_No):
+    student = Students.query.filter_by(admission_No=admission_No).first()
+    if 'file' not in request.files:
+        resp = jsonify({'message': 'No file part in the request'})
+        resp.status_code = 400
+        return resp
+    file = request.files['file']
+    if file.filename == '':
+        resp = jsonify({'message': 'No file part in the request'})
+        resp.status_code = 400
+        return resp
+    if file and allowed_file(file.filename):
+        filename = file.filename
+        file_extension = os.path.splitext(filename)[1]
+        filename = f'{admission_No}{file_extension}'
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        student.image_uploded = True
+        resp = jsonify({'message': 'File successfully uploaded'})
+        resp.status_code = 201
+        return resp
+    else:
+        resp = jsonify({'message': 'Allowed file types are png, jpg, jpeg'})
+        resp.status_code = 400
+        return resp
+
 # ---Marking Attendance Route---
-@app.route('/mark_attendance/<admission_No>/' , methods = ['PUT'])
+
+
+@app.route('/mark_attendance/<admission_No>/', methods=['POST'])
 def mark_attendance(admission_No):
-    student = Students.query.filter_by(admission_No = admission_No).first()
-    image_unknown = request.json['image_unknown']
-    present_status = request.json['present_status']
-    # Now decoding unknown img and saving it it unknown_img.png
-    image_64_decode = base64.b64decode(image_unknown)
-    unknown_img = open('unknown_img.jpeg' , 'wb')
-    unknown_img.write(image_64_decode)
-    # Now unknown_img contains image for testing
-    
+    student = Students.query.filter_by(admission_No=admission_No).first()
+    file = request.files['file']
+    filename = file.filename
+    path = 'static/ImgUploads'
+    images = []
+    classNames = []
+    myList = os.listdir(path)
+    for cl in myList:
+        curImg = cv2.imread(f'{path}/{cl}')
+        images.append(curImg)
+        classNames.append(os.path.splitext(cl)[0])
 
-    db.session.commit()
-    return student_schema.jsonify(student)
+    def findEncodings(images):
+        encodeList = []
+        for img in images:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            encode = face_recognition.face_encodings(img)[0]
+            encodeList.append(encode)
+        return encodeList
 
+    encodeListKnown = findEncodings(images)
 
+    file.save(os.path.join('static/', filename))
+    newpath = f'static/{filename}'
+    with open(newpath, "rb") as fid:
+        data = fid.read()
+    os.remove(os.path.join('static/', filename))
+    b64_bytes = base64.b64encode(data)
+    b64_string = b64_bytes.decode()
+    img = imread(io.BytesIO(base64.b64decode(b64_string)))
+    imgS = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
+    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            if name == admission_No:
+                student.present_status = True
+                db.session.commit()
+                return jsonify({'mesaage': 'Face Found'})
+
+            else:
+                return jsonify({'mesaage': 'Face NOT Found'})
+
+        else:
+            return jsonify({'mesaage': 'Face NOT Found'})
+    cv2.waitKey(1)
 
 
 if __name__ == "__main__":
